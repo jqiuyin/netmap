4eb6c5bfdd50aae96e77bde03185a017981eab40
qiuyin <woshiqiuyin@gmail.com>
2021-12-26 13:29:32 +0800

    fix some bug

--- app/api/netmap.py
+++ app/api/netmap.py
@@ -1,10 +1,15 @@
 from . import api
 from ..models import DataCenter, NetworkSegment
-from flask import request, jsonify
+from flask import request, jsonify, abort
+from .errors import bad_request
 
 @api.route('/datacenter')
 def get_datacenter():
+    if not request.args:
+        return bad_request('no network_segment')
     network_segment_name = request.args['network_segment']
     datacenter = DataCenter.query.join(NetworkSegment)\
         .filter(NetworkSegment.network_segment == network_segment_name).first()
+    if not datacenter: 
+        abort(404)
     return jsonify(datacenter.to_json())
